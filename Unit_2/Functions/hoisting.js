7f103d0f1f2f2df673448772b5d5e96cefa34c29
Sandore25 <sandore25@gmail.com>
2023-10-12 19:34:56 -0400

    updated hoisting notes

---
+++ Unit_2/Functions/hoisting.js
@@ -0,0 +1,28 @@
+/* 
+! Hoisting
+    - JS reads from top to bottom in two different passes.
+        1st pass:
+            - Made by the compiler. This is where hoisting is initiated.
+            - Compiled for execution.
+        2nd pass:
+            - Variables are assigned.
+            - Executes any hoisted code. 
+*/
+
+let names = 'Everly';
+console.log(names);
+
+b();
+
+function b() {
+    console.log("I have been hoisted!");
+}
+
+// c(); Cannot access 'c; before initialization error message
+
+let c = () => {
+    console.log("Hoisted?")
+}
+
+c();
+
